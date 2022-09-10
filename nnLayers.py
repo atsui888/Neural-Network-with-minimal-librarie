fix bias vanishing gradient issue
init at 0, dead neuron. hence changed to init at unity and allow bias_deltas to take effect.

nnLayers.py
@@ -84,8 +84,8 @@ class InputLayer(Layer):
         if self._layer_output.ndim == 1:
             self._layer_output = self._layer_output.reshape(-1, 1)
 
-        # init bias output matrix to 0, origin
-        self._layer_output_bias = np.zeros(self._data.shape[0] * 1).reshape(-1, 1)
+        # init bias output matrix to 1, unity
+        self._layer_output_bias = np.ones(self._data.shape[0] * 1).reshape(-1, 1)
 
         if self._debug_mode:
             print('\nInput Layer <-- set_data(self, data):')
@@ -117,10 +117,14 @@ class FullyConnectedLayer(Layer):
 
         # Init Weights
         # ************
-        # todo: comment out the next line when testing is over
+        #  method 1: fixed weights
         # self._layer_weights = np.full(self._nodes_prev_layer * self._nodes, fill_value=0.1)
-        # todo: use randomised weights (below) after testing is done (comment out above line)
-        self._layer_weights = np.random.rand(self._nodes_prev_layer * self._nodes)
+        # method 2: random from uniform distribution
+        # self._layer_weights = np.random.rand(self._nodes_prev_layer * self._nodes)
+        # method 3: random from normal distribution
+        # try this, if doesn't work, use the above
+        rng = np.random.default_rng(12345678)
+        self._layer_weights = rng.random((self._nodes_prev_layer * self._nodes))
 
         self._layer_weights = self._layer_weights.reshape(self._nodes_prev_layer, self._nodes)
         if self._layer_weights.ndim == 1:
@@ -133,7 +137,8 @@ class FullyConnectedLayer(Layer):
             print(f"Layer Name: {self.layer_name}")
             print(f"weights.shape:\t\t{self._layer_weights.shape}")
             print(f"bias.shape:\t\t{self._layer_bias.shape}")
-            print(f"layer_output.shape:\t{self._layer_output.shape}")
+            if self._layer_output is not None:
+                print(f"layer_output.shape:\t{self._layer_output.shape}")
 
     def get_layer_details(self):
         layer_details = super().get_layer_details()
@@ -231,7 +236,8 @@ class OutputRegression(FullyConnectedLayer):
             print('\nOutput Regression Layer <-- __init__ :')
             print(f"self._weights.shape: {self._layer_weights.shape}")
             print(f"self._bias.shape: {self._layer_bias.shape}")
-            print(f"self._layer_output.shape: {self._layer_output.shape:}")
+            if self._layer_output is not None:
+                print(f"self._layer_output.shape: {self._layer_output.shape:}")
 
     def predict(self, rounding=2):
         """

nnModel.py
@@ -110,6 +110,8 @@ class Model:
 
                     if epoch % print_threshold == 0 or epoch == epochs-1:
                         print(f"epoch #{epoch+1}: \tW.shape: {layer.get_weights_matrix().shape},\tB.shape: {layer.get_bias_matrix().shape}, \tCost: {cost:,.02f}")
+                        # print(f"Layer Weights:  \n{layer.get_weights_matrix()}")
+                        # print(f"Layer Bias:  \n{layer.get_bias_matrix()}\n")
 
                     # get_model_error() already done above
                     # Get Weight_Derivatives
@@ -120,7 +122,7 @@ class Model:
                     # updates its own weights. Similar concept for bias.
                     self.calc_weight_bias_deltas(
                         self._layers[idx-1].get_layer_output(),
-                        self._layers[idx].get_layer_output_bias()
+                        self._layers[idx-1].get_layer_output_bias()  # todo: prev it was [idx], but idx-1 seems correct
                     )
 
                     # todo: for perceptron this works, but what if there is >=1 hidden layer?
@@ -179,14 +181,24 @@ class Model:
 
         # shd be element multiply, not do product
         # https://numpy.org/doc/stable/reference/generated/numpy.multiply.html
+        # print(f"self._weight_deltas : \n{self._weight_deltas}")
+        # print(f"self._bias_deltas : \n{self._bias_deltas}")
+
         self._weight_deltas = np.sum(
             np.multiply(self._error_derivatives, layer_matrix),
             axis=0) / layer_matrix.shape[0]
         self._bias_deltas = np.sum(
-            np.multiply(self._error_derivatives, bias_matrix),
+            np.multiply(self._error_derivatives, bias_matrix),  # bias_matrix
             axis=0) / bias_matrix.shape[0]
 
-        # print(f"self._weight_deltas: \n{self._weight_deltas}")
+        if self._weight_deltas.ndim == 1:
+            self._weight_deltas = self._weight_deltas.reshape(-1, 1)
+        if self._bias_deltas.ndim == 1:
+            self._bias_deltas = self._bias_deltas.reshape(-1, 1)
+        # print(f"self._weight_deltas.shape --->: \n{self._weight_deltas.shape}")
+        # print(f"self._weight_deltas --->: \n{self._weight_deltas}")
+        # print(f"self._bias_deltas.shape --->: \n{self._bias_deltas.shape}")
+        # print(f"self._bias_deltas --->: \n{self._bias_deltas}")
 
     def save(self, file_name='my_nn_module.pkl'):
         """